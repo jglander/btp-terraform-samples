ead26bc704ef8139e608c4ca13ff51c1e692cd0b
Jens Glander <jens.glander@sap.com>
2024-06-20 15:52:49 +0200

    copied from cf multi stage

--- in-development/mission_4024_multi_step_test/1_main_config/outputs.tf
+++ in-development/mission_4024_multi_step_test/1_main_config/outputs.tf
@@ -1,4 +1,19 @@
-output "subaccount_id" {
-  value       = data.btp_subaccount.project.id
-  description = "The ID of the project subaccount."
+output "cf_landscape_labels" {
+  value = local.cf_landscape_labels
 }
+
+output "cf_api_url" {
+    value = jsondecode(btp_subaccount_environment_instance.cloudfoundry.labels)["API Endpoint"]
+}
+
+output "cf_org" {
+    value = btp_subaccount_environment_instance.cloudfoundry.platform_id
+}
+
+output "custom_ias_tenant" {
+    value = var.custom_ias_tenant
+}
+
+output "subaccount_id" {
+    value = btp_subaccount.project.id
+} 
